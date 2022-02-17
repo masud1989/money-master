b6e0c855f04db2f4e186399751c42b909a89ba6a
masud1989 <mranasrj@gmail.com>
2022-02-17 16:51:12 +0600

    Calculate cost and balance

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,17 @@
 // console.log('Yes, I am')
 
 //Gwetting Income 
-function getIncomeValue(){
-    const incomeInput = document.getElementById('inputIncome');
-    const incomeText =  incomeInput.value;
-    const income = parseFloat(incomeText);
-    incomeInput.value = '';
-    // return income;
+// function getIncomeValue(){
+//     const incomeInput = document.getElementById('inputIncome');
+//     const incomeText =  incomeInput.value;
+//     const income = parseFloat(incomeText);
+//     incomeInput.value = '';
+//      return income;
 
-}
+// }
 
 // Getting Expense
-function getExpense(){
+function calculate(){
     // getting food cost 
     const inputFoodCost = document.getElementById('foodCost');
     const inputFoodCostText = inputFoodCost.value;
@@ -27,35 +27,43 @@ function getExpense(){
 
     // getting Total Cost 
     const totalCost = parseFloat(foodCost) + parseFloat(rentCost) + parseFloat(clothCost);
+    const incomeInput = document.getElementById('inputIncome');
+    const incomeText =  incomeInput.value;
+    const myIncome = parseFloat(incomeText);
+    
+    const balance = myIncome - totalCost;
     const expenseTotal = document.getElementById('totalExpense');
-    const myIncome = document.getElementById('inputIncome');
+    const balanceTotal = document.getElementById('totalBalance');
+    // const balanceTotal = document.getElementById('totalBalance');
+    balanceTotal.innerText = balance;
     expenseTotal.innerText = totalCost;
+    incomeInput.value = '';
     inputFoodCost.value = '';
     inputRentCost.value = '';
     inputClothCost.value = '';
-
+  
     // return totalCost;
 }
  
 
-/*
-function calculate(){
-    const expenseTotal = document.getElementById('totalExpense');
-    // const balance = income - totalCost;
-    expenseTotal.innerText = totalCost;
-    getExpense()
 
-}
-*/
+// function calculate(){
+//     const expenseTotal = document.getElementById('totalExpense');
+//     // const balance = income - totalCost;
+//     expenseTotal.innerText = totalCost;
+//     getExpense()
+
+// }
+
  
 
 document.getElementById('calculate-btn').addEventListener('click', function(){
     // console.log('Calculate Button Clicked..');
-    const incomeTest = getIncomeValue();
+    // const incomeTest = getIncomeValue();
     
     // console.log(incomeTest);
 
-    const totalCostTest = getExpense();
+    const totalCostTest = calculate();
     // const 
     // const totalExpense = document.getElementById('totalExpense');
     // totalExpense.innerText = totalCost;
