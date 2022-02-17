62c00f074392fac5c3ba4c7b5de854e45aaa8b31
masud1989 <mranasrj@gmail.com>
2022-02-17 13:19:44 +0600

    Button click event added

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,8 @@
-console.log('Yes, I am')
+// console.log('Yes, I am')
+document.getElementById('calculate-btn').addEventListener('click', function(){
+    // console.log('Calculate Button Clicked..');
+})
+document.getElementById('save-btn').addEventListener('click', function(){
+    // console.log('Save Button Clicked..');
+})
+
