13e6c235d16b2f9983b16b77d2a3cd04b499a91c
masud1989 <mranasrj@gmail.com>
2022-02-17 13:41:19 +0600

    Input Catched

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,21 @@
 // console.log('Yes, I am')
+
+function getIncomeValue(){
+    const incomeInput = document.getElementById('inputIncome');
+    const incomeText =  incomeInput.value;
+    const income = parseFloat(incomeText);
+    incomeInput.value = '';
+    return income;
+   
+}
+
+
+
 document.getElementById('calculate-btn').addEventListener('click', function(){
     // console.log('Calculate Button Clicked..');
+    const incomeTest = getIncomeValue();
+    console.log(incomeTest);
+    
 })
 document.getElementById('save-btn').addEventListener('click', function(){
     // console.log('Save Button Clicked..');
